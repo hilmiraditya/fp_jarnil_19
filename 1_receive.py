8afe2a095fc83fb94495f8e8b2f8830bdc79e0a4
Hilmi Raditya <raditya113@icloud.com>
2019-12-12 00:59:26 +0700

    hehehehehehehe

---
+++ 1_receive.py
@@ -0,0 +1,78 @@
+import socket
+import struct
+import sys
+import time
+import json
+import ast
+
+id_receive = 'pc2'
+port = 10000
+multicast_ip = '224.3.29.71'
+
+def send(message): 
+    multicast_group = (multicast_ip, port)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.settimeout(0.2)
+
+    ttl = struct.pack('b', 1)
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+
+    sock.sendto(message, multicast_group)
+    print 'mengirimkan pesan... '
+
+    try:
+        sock.recvfrom(16)
+    except:
+        print "tidak ada respon"
+        sock.close()
+        return 0
+    else:
+        print "pesan berhasil dikirim"
+        sock.close()
+        return 1
+
+
+def checkWaktu(batas_waktu, timestamp):
+    waktuSekarang = time.time()
+    if(waktuSekarang - timestamp > batas_waktu):
+        print "durasi pengiriman melebihi limit waktu"
+        exit()
+        
+def checkId(id_pengirim):
+    if(id_pengirim == id_receive):
+        print 'Receive telah sesuai dengan tujuan awal'
+        return 1
+    else:
+        print 'Receive belum sesuai tujuan'
+        return 0
+
+def receive():
+    multicast_group = multicast_ip
+    server_address = ('', port)
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(server_address)
+
+    group = socket.inet_aton(multicast_group)
+    mreq = struct.pack('4sL', group, socket.INADDR_ANY)
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+    while True:
+        print 'menunggu pesan..'
+        data, address = sock.recvfrom(1024)
+        message = ast.literal_eval(data)
+
+        print >>sys.stderr, 'mengirim konfirmasi ke ', address
+        sock.sendto('ack', address)
+
+        cek_id = checkId(message[1])
+        if(cek_id == 1):
+            exit()
+        else:
+            check = 0
+            while(check != 1):
+                checkWaktu(int(message[3]),float(message[4]))
+                check = send(str(message))
+
+if __name__ == "__main__":
+    receive()
---
+++ send.py
@@ -0,0 +1,67 @@
+import socket
+import struct
+import sys
+import time
+import json
+
+port = 10000
+multicast_ip = '224.3.29.71'
+
+def send(message): 
+    multicast_group = (multicast_ip, port)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.settimeout(0.2)
+
+    ttl = struct.pack('b', 1)
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+
+    sock.sendto(message, multicast_group)
+    print 'mengirimkan pesan... '
+
+    try:
+        sock.recvfrom(16)
+    except:
+        print "tidak ada respon"
+        sock.close()
+        return 0
+    else:
+        print "pesan berhasil dikirim"
+        sock.close()
+        return 1
+
+def checkWaktu(batas_waktu, timestamp):
+    waktuSekarang = time.time()
+    if(waktuSekarang - timestamp > batas_waktu):
+        print "durasi pengiriman melebihi limit waktu"
+        exit()
+        
+if __name__ == "__main__":
+    message = []
+
+    # pesan = raw_input("Masukkan Pesan > ")
+    # id_tujuan = raw_input("Masukkan ID Tujuan > ")
+    # batas_hop = raw_input("Masukkan Batas Hop > ")
+    # batas_waktu = raw_input("Masukkan Batas Waktu > ")
+    # timestamp = time.time()
+
+    pesan = "memek"
+    id_tujuan = "pc1"
+    batas_hop = "30"
+    batas_waktu = "30"
+    timestamp = time.time()
+
+    #pesan
+    message.insert(0, pesan)
+    #id tujuan
+    message.insert(1, id_tujuan)
+    #batas hop
+    message.insert(2, int(batas_hop))
+    #batas waktu
+    message.insert(3, int(batas_waktu))
+    #timestamp
+    message.insert(4, timestamp)
+
+    check = 0
+    while(check != 1):
+        checkWaktu(int(batas_waktu), timestamp)
+        check = send(str(message))
